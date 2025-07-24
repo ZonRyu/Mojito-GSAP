468003d8a81a33368e4eb276dc7b4a1620de839d
julyonputra <julyonchang@gmail.com>
2025-07-24 14:06:16 +0700

    Navbar compo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import gsap from "gsap"
+import { navLinks } from "../../constans"
+import { useGSAP } from "@gsap/react"
+
+export const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        })
+    }, [])
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="logo" />
+                    <p>Mojito</p>
+                </a>
+
+                <ul>
+                    { navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
